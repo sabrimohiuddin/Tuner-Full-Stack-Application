bef05f483a64382eea55925e10e56ff6a2c1d829
Sabri Mohiuddin <you@example.com>
2023-08-02 21:44:07 -0400

    new update

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,15 @@
+
+
 \c songs_dev;
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Bad Guy', 'Billie Eilish', 'When We All Fall Asleep, Where Do We Go?', '3:14', true),
 ('Blinding Lights', 'The Weeknd', 'After Hours', '3:20', false),
-('Watermelon Sugar', 'Harry Styles', 'Fine Line', '2:54', true);
+('Watermelon Sugar', 'Harry Styles', 'Fine Line', '2:54', true),
+('Shape of You', 'Ed Sheeran', '÷ (Deluxe)', '3:53', true),
+('Bohemian Rhapsody', 'Queen', 'A Night at the Opera', '5:55', true),
+('Imagine', 'John Lennon', 'Imagine', '3:01', false),
+('Hotel California', 'Eagles', 'Hotel California', '6:30', true),
+('Stairway to Heaven', 'Led Zeppelin', 'Led Zeppelin IV', '8:02', false),
+('Smells Like Teen Spirit', 'Nirvana', 'Nevermind', '5:01', true),
+('Another Brick in the Wall, Pt. 2', 'Pink Floyd', 'The Wall', '3:59', false);
